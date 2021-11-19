61109c1e07847fc0f15f0308d970d45804556bc1
sotopingkal12 <91965362+sotopingkal12@users.noreply.github.com>
2021-11-19 15:33:15 +0700

    Update PresensiBerlangsung.php

--- app/Jobs/PresensiBerlangsung.php
+++ app/Jobs/PresensiBerlangsung.php
@@ -42,7 +42,7 @@ class PresensiBerlangsung implements ShouldQueue
 		
 		$pesan = '';
 		foreach ($data as $row) {
-			//if(strpos($row[4], 'Anda Belum Presensi') !== false) {
+			if(strpos($row[4], 'Anda Belum Presensi') !== false) {
 				$link = "https://ocw.uns.ac.id". explode('">',substr($row[4], strpos($row[4], 'href="') + 6))[0];
 				$link = str_replace('&amp;', '&', $link);
 				$pesan .= "
@@ -52,7 +52,7 @@ class PresensiBerlangsung implements ShouldQueue
 				Link : $link
 				=======================
 				";
-			//}
+			}
 		}
         
         
